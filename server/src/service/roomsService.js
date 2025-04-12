55d7b3fdf202cfad25d81fb2ee3fd14d0b3bfd6a
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-04-12 15:58:33 +0200

    added PostgreSQL system for messages

--- server/src/service/roomsService.js
+++ server/src/service/roomsService.js
@@ -1,29 +1,10 @@
-import { promises as fs } from 'fs';
 import { Room } from '../models/room.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { Message } from '../models/messsage.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const MESSAGES_URL = path.join(__dirname, 'messages.json');
-
-const readMessages = async () => {
-  try {
-    const data = await fs.readFile(MESSAGES_URL, 'utf-8');
-    return JSON.parse(data);
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      await fs.writeFile(MESSAGES_URL, '[]');
-      return [];
-    }
-    throw err;
-  }
-};
-
-const writeMessages = async (data) => {
-  await fs.writeFile(MESSAGES_URL, JSON.stringify(data, null, 2));
-};
 
 export const roomsService = {
   async getById(roomId) {
@@ -33,14 +14,10 @@ export const roomsService = {
   async create(userName, roomName, limit) {
     const created = await Room.create(
       { admin: userName, name: roomName, limit, users: [userName] },
-      { returning: true }
+      { returning: true },
     );
 
     if (!created) return false;
-
-    const allMessages = await readMessages();
-    allMessages.push({ id: created.id, messages: [] });
-    await writeMessages(allMessages);
 
     return created;
   },
@@ -63,36 +40,19 @@ export const roomsService = {
   },
 
   async getMessages(roomId) {
-    const allMessages = await readMessages();
-    let room = allMessages.find((r) => r.id === roomId);
+    const allMessages = await Message.findAll({ where: { roomId } });
 
-    if (!room) {
-      room = { id: roomId, messages: [] };
-      allMessages.push(room);
-      await writeMessages(allMessages);
-    }
-
-    return room.messages;
+    return allMessages.map((message) => message.dataValues);
   },
 
   async addMessage(roomId, user, content) {
-    const allMessages = await readMessages();
-    let room = allMessages.find((r) => r.id === roomId);
+    const messageDate = new Date().toISOString();
 
-    if (!room) {
-      room = { id: roomId, messages: [] };
-      allMessages.push(room);
-    }
+    const newMessage = await Message.create(
+      { text: content, name: user, roomId: roomId, date: messageDate },
+      { returning: true },
+    );
 
-    const newMessage = {
-      name: user,
-      text: content,
-      date: new Date().toISOString(),
-    };
-
-    room.messages.push(newMessage);
-    await writeMessages(allMessages);
-
-    return newMessage;
-  }
+    return newMessage.dataValues;
+  },
 };
